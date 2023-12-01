2525e71414dca69c7b91f90e53e48d67e0dc95f4
ryliewalsh <62161188+ryliewalsh@users.noreply.github.com>
2023-11-30 22:22:36 -0700

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.1.5"
+    id("io.spring.dependency-management") version "1.1.3"
+}
+
+group = "com.heroku"
+version = "1.0.0-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-jdbc")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.postgresql:postgresql")
+}
